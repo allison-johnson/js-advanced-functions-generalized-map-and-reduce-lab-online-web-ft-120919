286c3b57a173513e7afd8bad7731c36751d23ebe
allison_johnson <allison.cuttler@gmail.com>
2020-04-04 13:20:36 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Add your functions here
+let map = function(arr, fn){ 
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++){
+    newArr.push(fn(arr[i]));
+  }
+  return newArr;
+}
+
+let reduce = function(arr, fn, start){
+
+}
